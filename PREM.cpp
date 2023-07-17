34ae4082bc5da38acf1d1709c71096c4ec2b23ae
PremSupasrisan <139094326+PremSupasrisan@users.noreply.github.com>
2023-07-17 10:10:36 +0700

    Add files via upload

---
+++ PREM.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string Name;
+	int birth ;
+	cout << "Enter Name : ";
+	cin  >> Name;
+	cout << "Enter year of birth : ";
+	cin  >> birth;
+	cout << "Your Age =  " <<2023-birth<< " years old\n ";
+	system("pause");
+    return(0);
+}

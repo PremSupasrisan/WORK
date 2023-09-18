2f377955113181e9087706ed866e54d28de5dfcb
PremSupasrisan <139094326+PremSupasrisan@users.noreply.github.com>
2023-09-18 12:01:08 +0700

    Add files via upload

--- array.cpp
+++ array.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <string>
 using namespace std;
+string check_result( int x);
 int main()
 {
 	int num[4], total = 0 , max = INT_MIN , min = INT_MAX ;// (INT_MAX, INT_MIN) ใช้กำหนดค่าสูงสุดและต่ำสุดของ INT
@@ -14,11 +15,13 @@ int main()
 		cin >> num[i];
 		
 	}
-	cout << "---------------------------\n";
-	cout << "Name		Score\n";
-	cout << "---------------------------\n";
+	cout << "-------------------------------------------------\n";
+	cout << "Name		Score		Result\n";
+	cout << "-------------------------------------------------\n";
 	for (int i = 0 ; i < 4 ; i++){
-	cout << name[i] << "\t\t"<< num[i]<<endl;
+	cout << name[i] << "\t\t"<< num[i]<<"\t\t"<<check_result(num[i])<<endl;
+	
+	
 	total =  total+ num[i];
 	if (num[i] < min)
 		min = num[i];
@@ -31,4 +34,12 @@ int main()
 	cout << "Average Score = " << (float)total/4 <<endl;
 	system("pause");
 	return 0;
+}
+
+string check_result( int x)
+{
+	if (x >= 50)
+		return("Pass");
+	else 
+		return("Failed");
 }
